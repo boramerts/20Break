eebbf03e8ef092c08da8b68119ec809515958fdd
boramerts <mertbora723@gmail.com>
2025-04-04 23:18:25 +0300

    Initial Commit

---
+++ LookAway/LookAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookAwayApp.swift
+//  LookAway
+//
+//  Created by Bora Mert on 4.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LookAwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
